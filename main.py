2707846cf1de5d139e2922fbca941097efeda988
mullagalievdeniz <mullagalievdeniz@yandex.ru>
2024-06-20 23:19:47 +0300

    day2 - > context support

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openai
 import aiomysql
 from google.cloud import storage
 from buttons import markup
-
+import json
 
 TOKEN = ''
 OPENAI_TOKEN = ''
@@ -20,9 +20,77 @@ async def start(message: types.Message):
     await message.reply('Привет!\nЯ бот на основе ИИ', reply_markup=markup)
 
 
+@dp.message_handler():
+async def default_handler(message: types.Message):
+    answer = await generate_text(quest=message.text, model='gpt4o', id=message.from_user.id)
+
+
 @dp.message_handler(lambda message: message.text == "GPT-Vision")
 async def hi_reply(message: types.Message):
     await message.reply("Режим Vison включён, отправь мне фото")
+    
+
+
+async def get_data_from_context(id: int):
+    db_config = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'user',
+        'password': 'pass',
+        'db': 'context',
+    }
+    connection = await aiomysql.connect(**db_config)
+    try:
+        async with connection.cursor() as cursor:
+            await cursor.execute("SELECT data FROM context WHERE id = %s", (id,))
+            result = await cursor.fetchone()
+            if result:
+                data = json.loads(result[0])
+                if len(data) > 6:
+                    del data[0]
+                    del data[1]
+                return data
+            else:
+                data.append({"role": "system", "content": "You are a helpful assistant."})
+                return data
+    finally:
+        connection.close()
+
+
+async def save_context(id: int, data: list):
+    db_config = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'user',
+        'password': 'pass',
+        'db': 'context',
+    }
+    connection  = await aiomysql.connect(**db_config)
+    try:
+        async with connection.cursor() as cursor:
+            await cursor.execute(
+                "UPDATE context SET data = %s WHERE id = %s",
+                (data, id)
+            )
+            await connection.commit()
+    finally:
+        connection.close()
+
+
+
+
+async def generate_text(quest, model, id) -> str:
+    history = await get_data_from_context(id)
+    history.append({"role": "user", "content": quest})
+    response  = await openai_client.chat.completions.create(
+        model = model,
+        messages=history
+    )
+    result = response.choices[0].message.content
+    context_part = {"role": "assistant",  "content":  result}
+    history.append(context_part)
+    await save_context(id, history)
+    return result
     
 
 
